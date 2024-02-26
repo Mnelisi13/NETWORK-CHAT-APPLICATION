ecc87717e7fe3f07b80385aeb8a872a48ab9fa55
Mnelisi <mbzmne001@myuct.ac.za>
2024-02-26 16:17:40 +0200

    newbnew

--- myClient.py
+++ myClient.py
@@ -1,12 +1,30 @@
 import socket
-#Client
-#UDP messaging
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message, address = client.recvfrom(1024)[0]
-print(message.decode('utf-8'))
-client2IP = input("Enter the client you want to send a message to's IP address\n")
-client2Port =eval(input("Enter the port number of the client:\n"))
-messageTo =input("Enter the message: ")
-client.sendto(messageTo.encode(), (client2IP, client2Port))
- 
-#Add connection to server using TCP!
+
+HOST = "196.47.247.212"# Host IP address
+PORT = 9090
+ADDR = (HOST, PORT)
+SIZE = 1024
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "Disconnect".lower()
+
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print(f"[CONNECTED] Client connected to server at {HOST}:{PORT}")
+
+    connected = True
+    while connected:
+        msg = input("Enter a message (type 'disconnect' to disconnect from server OR type \"connections\" to view active clients):\n")
+        client.send(msg.encode(FORMAT))
+        if msg == DISCONNECT_MSG:
+            connected = False
+
+        else:
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"Message from server {ADDR}: {msg} ")
+
+
+if __name__ == "__main__":
+    main()
